e0047fb3a7d1dbdfd4c364477315012225cb8a5f
nko2005 <nko2005@nyu.edu>
2024-11-24 22:04:26 +0800

    assignment6

--- my-nextjs-app/src/pages/index.jsx
+++ my-nextjs-app/src/pages/index.jsx
@@ -1,19 +1,21 @@
 import Head from 'next/head';
 import Assignment4Component from './assignment4_nawaf';
-import Assignment5Component from './assignment5_nawaf'; 
+// import Assignment5Component from './assignment5_nawaf'; 
+import Assignment6Component from './assignment6_student';
 export default function Home() {
   return (
     <>
       <Head>
         <title>Assignment 4 and 5 Visualization</title>
-        <meta name="description" content="Assignment 4 and 5 Visualization using D3 and React" />
+        <meta name="description" content="Assignment 4 & 5 & 6 Visualization using D3 and React" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Assignment 4 and 5 Visualization</h1>
+        <h1>Assignment 4 & 5 & 6 Visualization</h1>
         <Assignment4Component />
-        <Assignment5Component />
+        {/* <Assignment5Component /> */}
+        <Assignment6Component />
       </main>
     </>
   );
